683abe690d8f8bb08a9b08a10044170e6fdb9c0f
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2025-04-24 09:24:55 -0300

    update

--- projeto_maturacao/scripts/03_S_color_carapace-type.R
+++ projeto_maturacao/scripts/03_S_color_carapace-type.R
@@ -46,9 +46,6 @@ claw <- reflet_claw %>%
   mutate(media2 = mean(c_across(contains("Dark")), na.rm = TRUE),
          sd2 = sd(c_across(contains("Dark")), na.rm = TRUE)) %>%
   
-  mutate(media3 = mean(c_across(contains("Bright")), na.rm = TRUE),
-         sd3 = sd(c_across(contains("Bright")), na.rm = TRUE)) %>%
-  
   ungroup() %>%
   
   ggplot(aes(x = wl)) +
@@ -59,22 +56,16 @@ claw <- reflet_claw %>%
   geom_ribbon(aes(ymin = media2 - sd2, ymax = media2 + sd2, fill = "Dark"), alpha = 0.3, show.legend = FALSE) +
   geom_line(aes(y = media2, color = "Dark"), size = 1) +
   
-  geom_ribbon(aes(ymin = media3 - sd3, ymax = media3 + sd3, fill = "Darkened white"), alpha = 0.3, show.legend = FALSE) +
-  geom_line(aes(y = media3, color = "Darkened white"), size = 1) +
-  
   scale_fill_manual(values = c("White" = "#ffb560", 
-                               "Dark" = "black",
-                               "Darkened white" = "grey30"), name = "") +
+                               "Dark" = "black"), name = "") +
   scale_color_manual(values = c("White" = "#ffb560", 
-                                "Dark" = "black",
-                                "Darkened white" = "grey30"), name = "") +
+                                "Dark" = "black"), name = "") +
   labs(x = "Wavelenght (nm)",
        y = "Reflectance (%)") +
-  theme_classic(base_size = 14) +
+  theme_classic(base_size = 11) +
   theme(legend.position = "top") +
   geom_text(x = 500, y = 55, label = "Claw", color = "black", size = 4)
 claw
-
 
 #Plot means - carapace ----
 cara <- reflet_cara %>%
@@ -86,9 +77,6 @@ cara <- reflet_cara %>%
   mutate(media2 = mean(c_across(contains("Dark")), na.rm = TRUE),
          sd2 = sd(c_across(contains("Dark")), na.rm = TRUE)) %>%
   
-  mutate(media3 = mean(c_across(contains("Bright")), na.rm = TRUE),
-         sd3 = sd(c_across(contains("Bright")), na.rm = TRUE)) %>%
-  
   ungroup() %>%
   
   ggplot(aes(x = wl)) +
@@ -99,125 +87,51 @@ cara <- reflet_cara %>%
   geom_ribbon(aes(ymin = media2 - sd2, ymax = media2 + sd2, fill = "Dark"), alpha = 0.3, show.legend = FALSE) +
   geom_line(aes(y = media2, color = "Dark"), size = 1) +
   
-  geom_ribbon(aes(ymin = media3 - sd3, ymax = media3 + sd3, fill = "Darkened white"), alpha = 0.3, show.legend = FALSE) +
-  geom_line(aes(y = media3, color = "Darkened white"), size = 1) +
-  
   scale_fill_manual(values = c("White" = "#ffb560", 
-                               "Dark" = "black",
-                               "Darkened white" = "grey30"), name = "") +
+                               "Dark" = "black"), name = "") +
   scale_color_manual(values = c("White" = "#ffb560", 
-                                "Dark" = "black",
-                                "Darkened white" = "grey30"), name = "") +
+                                "Dark" = "black"), name = "") +
   labs(x = "Wavelenght (nm)",
        y = "Reflectance (%)") +
-  theme_classic(base_size = 14) +
+  theme_classic(base_size = 11) +
   theme(legend.position = "top") +
   geom_text(x = 400, y = 50, label = "Carapace", color = "black", size = 4)
 cara
-
-#Colspace ----
-sens_uca <- pavo::sensmodel(c(430, 520), range = c(300, 700))
-sens_uca <- pavo::as.rspec(sens_uca, lim =c(300, 700))
-transmit <- transmit #from colorspec package
-
-cara_qi   <- pavo::vismodel(reflet_cara, qcatch = "Qi", 
-                            visual = sens_uca, 
-                            achromatic = "l",
-                            illum = "D65",
-                            trans = "ideal", 
-                            scale = 1, 
-                            relative = T)
-
-claw_qi   <- pavo::vismodel(reflet_claw, qcatch = "Qi", 
-                            visual = sens_uca, 
-                            achromatic = "l",
-                            illum = "D65",
-                            trans = "ideal", 
-                            scale = 1, 
-                            relative = T)
-
-#Dichromatic Colspace ----
-colspace_cara <- colspace(cara_qi, space = "di")
-colspace_cara$ID <- rownames(colspace_cara)
-
-colspace_claw <- colspace(claw_qi, space = "di")
-colspace_claw$ID <- rownames(colspace_claw)
-
-plot(colspace_cara, 
-     cex = 1.5, 
-     col = ifelse(grepl("Bright", colspace_cara$ID), "#ffb560","#404244"))
-plot(colspace_claw, 
-     cex = 1.5,
-     col = ifelse(grepl("Bright", colspace_claw$ID), "#ffb560","#404244"))
 
 # Colorimetrics ----
 data_brac2 <- data_brac %>%
   mutate(carapace_type = fct_recode(carapace_type,
                                     "White" = "Real",
                                     "Dark" = "Dark",
-                                    "Darkened white" = "Bright"))
-
-b2 <- ggplot(data_brac2, aes(carapace_type, mean_brightness, fill = carapace_type))+
-  stat_halfeye(alpha = 0.5, justification = 0, width = 0.5, .width = 0, adjust = 0.8)+
-  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 1.1)+
-  geom_boxplot(width = 0.15, show.legend = F, fill = "white")+
-  scale_fill_manual(values=c("grey30","black","#ffb560"))+
-  scale_color_manual(values=c("grey30","black","#ffb560"))+
-  labs(x = "",
-       y = "Mean brightness")+
-  theme_classic(base_size = 14)+
-  theme(legend.position = "none")+
-  facet_grid(~body_region)+
-  scale_y_continuous(limits = c(0, 80))
-b2
-
-sat <- ggplot(data_brac2, aes(carapace_type, saturation, fill = carapace_type))+
-  stat_halfeye(alpha = 0.5, justification = 0, width = 0.5, .width = 0, adjust = 0.8)+
-  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 0.04)+
-  geom_boxplot(width = 0.15, show.legend = F, fill = "white")+
-  scale_fill_manual(values=c("grey30","black","#ffb560"))+
-  scale_color_manual(values=c("grey30","black","#ffb560"))+
-  labs(x = "",
-       y = "Saturation")+
-  theme_classic(base_size = 14)+
-  theme(legend.position = "none")+
-  facet_grid(~body_region)+
-  scale_y_continuous(limits = c(0, 2.5))
-sat
+                                    "Darkened white" = "Bright")) %>% 
+  filter(carapace_type != "Darkened white")
 
 #Figure 3 ----
-ds <- ggplot(data_brac2, aes(carapace_type, chromatic_contrast, fill = carapace_type))+
-  geom_hline(yintercept = 1, linetype = "dashed", color = "grey", size = 0.5)+
-  geom_hline(yintercept = 3, linetype = "dashed", color = "grey", size = 0.5)+
+s <- ggplot(data_brac2, aes(carapace_type, s, fill = carapace_type))+
   stat_halfeye(alpha = 0.5, justification = 0, width = 0.5, .width = 0, adjust = 1)+
-  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 0.07)+
+  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 0.006)+
   geom_boxplot(width = 0.15, show.legend = F, fill = "white")+
-  scale_fill_manual(values=c("grey30","black","#ffb560"))+
-  scale_color_manual(values=c("grey30","black","#ffb560"))+
-  labs(x = "",
-       y = "Chromatic contrast (JND)")+
+  scale_fill_manual(values=c("grey30","#ffb560"))+
+  scale_color_manual(values=c("grey30","#ffb560"))+
+  labs(x = "Carapace color",
+       y = "Quantum catch (S-receptor)")+
   theme_classic(base_size = 11)+
   theme(legend.position = "none")+
-  facet_grid(~body_region)+
-  scale_y_continuous(limits = c(0, 4.5))
-ds 
+  facet_grid(~body_region)
+s 
 
-dl <- ggplot(data_brac2, aes(carapace_type, achromatic_contrast, fill = carapace_type))+
-  geom_hline(yintercept = 1, linetype = "dashed", color = "grey", size = 0.5)+
-  geom_hline(yintercept = 3, linetype = "dashed", color = "grey", size = 0.5)+
+m <- ggplot(data_brac2, aes(carapace_type, m, fill = carapace_type))+
   stat_halfeye(alpha = 0.5, justification = 0, width = 0.5, .width = 0, adjust = 1)+
-  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 0.04)+
+  stat_dots(aes(color = carapace_type),side = "left",justification = 1, binwidth = 0.009)+
   geom_boxplot(width = 0.15, show.legend = F, fill = "white")+
-  scale_fill_manual(values=c("grey30","black","#ffb560"))+
-  scale_color_manual(values=c("grey30","black","#ffb560"))+
-  labs(x = "",
-       y = "Achromatic contrast (JND)")+
+  scale_fill_manual(values=c("grey30","#ffb560"))+
+  scale_color_manual(values=c("grey30","#ffb560"))+
+  labs(x = "Carapace color",
+       y = "Quantum catch (M-receptor)")+
   theme_classic(base_size = 11)+
   theme(legend.position = "none")+
-  facet_grid(~body_region)+
-  scale_y_continuous(limits = c(0, 2.5))
-dl 
-
+  facet_grid(~body_region)
+m
 
 #plot grid ----
 p <- plot_grid(claw,cara,
@@ -227,62 +141,23 @@ p <- plot_grid(claw,cara,
 p
 
 p2 <- plot_grid(p,
-                b2, sat,
+                s, m,
                ncol = 1,
                labels = "AUTO",
                align = "vh",
                label_size = 15)
 p2
 
-p3 <- plot_grid(ds, dl,
-                ncol = 1,
-                labels = "AUTO",
-                align = "vh",
-                label_size = 14)
-p3
-
 #Saving plots ----
 
 #reflectances ----
-ggsave(plot = p, 
-       filename = "outputs/figures/figure_1.png",
-       width = 7, 
-       height = 3.5, 
-       dpi = 300)
-
 ggsave(plot = p2, 
-       filename = "outputs/figures/figure_2.png",
-       width = 7.5, 
-       height = 8.5, 
+       filename = "outputs/figures/figure_1.png",
+       width = 6, 
+       height = 8, 
        dpi = 300)
 
-ggsave(plot = p3, 
-       filename = "outputs/figures/figure_3.png",
-       width = 5, 
-       height = 5.5, 
-       dpi = 300)
 
-#Colspaces ----
-png(file = "outputs/figures/01_colspace_cara.png",
-    units = "in",
-    res = 300,
-    width = 4.5,
-    height = 4)
+# FIM ---------------------------------------------------------------------
 
-plot(colspace_cara, cex = 1.5, 
-     col = ifelse(grepl("Bright", colspace_cara$ID), "#ffb560","#404244"))
 
-dev.off()
-
-png(file = "outputs/figures/01_colspace_claw.png",
-    units = "in",
-    res = 300,
-    width = 4.5,
-    height = 4)
-
-plot(colspace_claw, cex = 1.5, 
-     col = ifelse(grepl("Bright", colspace_claw$ID), "#ffb560","#404244"))
-
-dev.off()
-
-#THE END ----
